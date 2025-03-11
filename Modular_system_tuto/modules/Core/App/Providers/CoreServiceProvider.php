fac66c48e1e4a3f2b44a68a00548ad158c4cab93
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-03-11 13:49:20 +0000

    update the provider to load the migrations routes and views

---
+++ Modular_system_tuto/modules/Core/App/Providers/CoreServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\Core\App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // Load migrations
+        $this->loadMigrationsFrom(__DIR__ . '/../../Database/Migrations');
+
+        // Load all route files in the Routes directory
+        foreach (glob(__DIR__ . '/../../Routes/*.php') as $routeFile) {
+            $this->loadRoutesFrom($routeFile);
+        }
+
+        // Load views
+        $this->loadViewsFrom(__DIR__ . '/../../Resources/Views', 'Core');
+    }
+}
